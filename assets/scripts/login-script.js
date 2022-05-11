d17f86b040253bf7e2a07d276ac9f97fbbaee89a
elfath173 <105049877+elfath173@users.noreply.github.com>
2022-05-11 10:00:48 +0700

    Update login-script.js
    Mengganti komentar dengan format Jsdoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -23,3 +23,69 @@ loginFormElement.addEventListener('submit', function(event) {
     showPopUp();
   }
 });
+Skip to content
+Search
+Pulls
+Issues
+Marketplace
+Explore
+ 
+@elfath173 
+elfath173
+/
+Aplikasi-Login-1
+Public
+forked from dicodingacademy/Aplikasi-Login
+Code
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+Aplikasi-Login-1/assets/scripts/login-script.js
+@gilangadhan
+gilangadhan Merge branch '03-starter-submission' into 02-solution-study-case
+Latest commit a073c96 on 9 Jun 2021
+ History
+ 2 contributors
+25 lines (21 sloc)  783 Bytes
+ 
+/*
+ * Catatan:
+ * Gunakan fungsi goToHome() untuk menampilkan halaman home
+ * Gunakan fungsi goToLogin() untuk menampilkan halaman login
+ * Gunakan fungsi showPopUp() untuk menampilkan pop up error
+ */
+
+const loginFormElement = document.querySelector('#loginForm');
+const inputEmailElement = document.querySelector('#inputEmail');
+const inputPasswordElement = document.querySelector('#inputPassword');
+
+const expectedEmail = 'admin@dicoding.com';
+const expectedPassword = 'superpassword';
+
+loginFormElement.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const email = inputEmailElement.value;
+  const password = inputPasswordElement.value;
+  if (email == expectedEmail && password == expectedPassword) {
+    goToHome();
+  } else {
+    showPopUp();
+  }
+});
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
